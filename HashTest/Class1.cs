f493c43949fa127b7e8f7999f63a54884c894744
Guillaume Turri <g.turri@criteo.com>
2015-03-12 18:39:50 +0100

    Added test project (for real)

---
+++ HashTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTest
+{
+    public class Class1
+    {
+    }
+}
