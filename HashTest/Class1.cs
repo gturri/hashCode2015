a2ba3e933fedfa0154e1780098267bea028f0f17
Guillaume Turri <g.turri@criteo.com>
2015-03-12 20:19:15 +0100

    Can check if a solution is valid

--- HashTest/Class1.cs
+++ HashTest/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HashCode;
+using NUnit.Framework;
 
 namespace HashTest
 {
     public class Class1
     {
+        [Test]
+        public void Dumper()
+        {
+        }
     }
 }
---
+++ Scorer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HashCode
+{
+    class Scorer
+    {
+        private readonly Problem _problem;
+
+        public Scorer(Problem problem)
+        {
+            _problem = problem;
+        }
+
+        public bool IsValid(List<ServerInSlot> solution)
+        {
+            for (int i = 0; i < solution.Count(); i++)
+            {
+                ServerInSlot server = solution[i];
+
+                int rowIdx = server.IdxRow;
+                int colIdx = server.IdxCol;
+
+                for (int j = 0; j < server.Server.Size; j++)
+                {
+                    if (!_problem.IsSlotAvailable(rowIdx, colIdx + j))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
