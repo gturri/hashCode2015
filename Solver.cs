e4e7f4b404ec58fa79d028a84551c0a7e9db3f4f
Alexandre Olivier <a.olivier@criteo.com>
2015-03-12 20:39:08 +0100

    Fixed out of range

--- Solver.cs
+++ Solver.cs
@@ -8,33 +8,33 @@ namespace HashCode
     class Solver
     {
         private readonly Problem problem;
-        private readonly List<List<bool>> dataCenter = new List<List<bool>>(); // matrix representing sloting in dc : true means available, false filled
+        private readonly bool[,] dataCenter; // matrix representing sloting in dc : true means available, false filled
         private readonly List<ServerInSlot> slotedServer = new List<ServerInSlot>(); 
 
 
         public Solver(Problem problemStatement)
         {
             problem = problemStatement;
+            // fill dataCenter with empty and unavailable slots
+            dataCenter = new bool[problemStatement.NbRows, problemStatement.NbSlotsPerRows];
+            for (int i = 0; i < problem.NbRows; i++)
+                for (int j = 0; j < problem.NbSlotsPerRows; j++)
+                    dataCenter[i,j] = problem.IsSlotAvailable(i, j);
         }
 
 
         public void Solve()
         {
-            // fill dataCenter with empty and unavailable slots
-            for (int i = 0; i < problem.NbRows; i++)
-                for (int j = 0; j < problem.NbSlotsPerRows; j++)
-                    dataCenter[i][j] = problem.IsSlotAvailable(i, j);
-
-
-
-            // Filling Strategy
+            // -------------
+            // Strategy
             // Assume that servers physical size ~ server capacity (wrong, but not slightly related to reality given the dataset)
             // Step 1 : fill the rows with servers, with priority to dispersing servers over many rows (stupid but heh)
             // Step 2 : set servers in groups to optimize capacity
-
+            // -------------
 
             // Step 1 : place each servers in an available slot
             // todo : fill to balance over every row ! not fill rows first like its done now. (req !)
+            // todo : randomize foreach, evaluate solution and submit the best one (good optimize approach)
             // todo : do not iterate over the whole matrix everytime (if time)
             foreach (var server in problem.Servers)
             {
@@ -52,7 +52,7 @@ namespace HashCode
                         bool slotable = true;
                         for (int k = 0; k < server.Size; k++)
                         {
-                            if (!dataCenter[i][j + k]) // cannot slot here
+                            if (!dataCenter[i,j + k]) // cannot slot here
                             {
                                 slotable = false;
                                 break;
@@ -62,7 +62,7 @@ namespace HashCode
                         {
                             slotedServer.Add(new ServerInSlot(server, -1, i, j)); // add our server in the "done" list
                             for (int k = 0; k < server.Size; k++) // set the space as unavailable in the dc
-                                dataCenter[i][j + k] = false;
+                                dataCenter[i,j + k] = false;
                             serverIsSloted = true;
                         }
 
@@ -70,8 +70,8 @@ namespace HashCode
                 }
             }
 
-            // Step 2 : servers are now in place. We will now select groups to maximize availability
-                    
+            // Step 2 : servers are now in place. We will now assign groups to maximize availability
+            
 
 
 
