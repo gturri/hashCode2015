20aebb66bde857e1739e71dea2f1a849545ee4da
Raphael Vandon <r.vandon@criteo.com>
2015-03-12 16:54:44 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HashCode
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
