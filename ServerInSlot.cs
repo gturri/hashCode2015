86ecffd8dd861868b91d086b6168ff25422d923c
Alexandre Olivier <a.olivier@criteo.com>
2015-03-12 19:51:13 +0100

    Added solver steps.

---
+++ ServerInSlot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HashCode
+{
+    class ServerInSlot
+    {
+        private Server server;
+        private int group;
+        private int IdxRow;
+        private int IdxCol;
+
+        public ServerInSlot(Server serv, int groupId, int row, int col)
+        {
+            server = serv;
+            group = groupId;
+            IdxRow = row;
+            IdxCol = col;
+        }
+
+
+    }
+}
--- Solver.cs
+++ Solver.cs
@@ -8,7 +8,9 @@ namespace HashCode
     class Solver
     {
         private readonly Problem problem;
-        private List<List<Slot>> dataCenter;
+        private readonly List<List<Slot>> dataCenter = new List<List<Slot>>();
+        private readonly List<ServerInSlot> slotedServer = new List<ServerInSlot>(); 
+
 
         public Solver(Problem problemStatement)
         {
@@ -18,13 +20,37 @@ namespace HashCode
 
         public void Solve()
         {
-            dataCenter = new List<List<Slot>>();
             // fill dataCenter with empty and unavailable slots
             for (int i = 0; i < problem.NbRows; i++)
                 for (int j = 0; j < problem.SlotsPerRows; j++)
                     dataCenter[i][j] = problem.IsSlotAvailable(i, j) ? new Slot(i, j, false) : new Slot(i, j, true);
 
 
+
+            // Filling Strategy
+            // Assume that servers physical size ~ server capacity (wrong, but not slightly related to reality given the dataset)
+            // Step 1 : fill the rows with servers, with priority to dispersing servers over many rows (stupid but heh)
+            // Step 2 : set servers in groups to optimize capacity
+
+
+            // Step 1 : place each servers in an available slot
+            foreach (var server in problem.Servers)
+            {
+                int serverRow = -1;
+                int serverCol = -1;
+
+                // find an empty slot
+                foreach (var nbRow in dataCenter)
+                {
+                    
+                }
+
+                // update dc status
+                // update server status
+                slotedServer.Add(new ServerInSlot(server, -1, serverRow, serverCol));
+            }
+
+            // Step 2 : servers are now in place. We will now select groups to maximize availability
                     
 
 
