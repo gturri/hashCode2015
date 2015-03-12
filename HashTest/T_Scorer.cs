954b1d2fb2ede0ce1ba7db906504085c1b361a58
Guillaume Turri <g.turri@criteo.com>
2015-03-12 21:03:07 +0100

    Tested computeScore

--- HashTest/T_Scorer.cs
+++ HashTest/T_Scorer.cs
@@ -43,5 +43,32 @@ namespace HashTest
             solution.Add(new ServerInSlot(new Server(1, 1, 1), 0, 0, 2));
             Assert.True(_scorer.IsValid(solution));
         }
+
+        [Test]
+        public void ComputeCorrectScore1()
+        {
+            List<ServerInSlot> solution = new List<ServerInSlot>();
+            solution.Add(new ServerInSlot(new Server(capacity: 3, size: 1, idx: 0), groupId: 0, row: 0, col: 0));
+            solution.Add(new ServerInSlot(new Server(capacity: 2, size: 1, idx: 1), groupId: 0, row: 1, col: 0));
+
+            solution.Add(new ServerInSlot(new Server(capacity: 5, size: 1, idx: 2), groupId: 1, row: 0, col: 4));
+            solution.Add(new ServerInSlot(new Server(capacity: 4, size: 1, idx: 3), groupId: 1, row: 1, col: 4));
+
+            Assert.AreEqual(2, _scorer.ComputeScore(solution));
+        }
+
+        [Test]
+        public void ComputeCorrectScore2()
+        {
+            List<ServerInSlot> solution = new List<ServerInSlot>();
+            solution.Add(new ServerInSlot(new Server(capacity: 3, size: 1, idx: 0), groupId: 0, row: 0, col: 0));
+            solution.Add(new ServerInSlot(new Server(capacity: 2, size: 1, idx: 1), groupId: 0, row: 1, col: 0));
+            solution.Add(new ServerInSlot(new Server(capacity: 4, size: 1, idx: 2), groupId: 0, row: 1, col: 1));
+
+            solution.Add(new ServerInSlot(new Server(capacity: 5, size: 1, idx: 3), groupId: 1, row: 0, col: 4));
+            solution.Add(new ServerInSlot(new Server(capacity: 4, size: 1, idx: 4), groupId: 1, row: 1, col: 4));
+
+            Assert.AreEqual(3, _scorer.ComputeScore(solution));
+        }
     }
 }
