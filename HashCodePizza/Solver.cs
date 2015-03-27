f04a4bdb7a520e70395e41c4563f7bd8b80a3895
Alexandre Olivier <a.olivier@criteo.com>
2015-03-27 21:08:16 +0100

    Basic solver

--- HashCodePizza/Solver.cs
+++ HashCodePizza/Solver.cs
@@ -11,8 +11,35 @@ namespace HashCodePizza
         public void Solve(bool[,] pizza)
         {
             var solution = new List<Slice>();
+            bool[,] cutSpace = new bool[,];
 
 
+            for (int i = 0; i < pizza.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pizza.GetLength(1); j++)
+                    {             
+                        
+                        var slice = new Slice();
+                        int jambonCounter = 0;
+                        int sliceSize = 0;
+
+                        // Add this case
+                        sliceSize++;
+                        if (pizza[i,j])
+                            jambonCounter++;
+
+                        // cut slice if done
+                        if (jambonCounter == 3)
+                        {
+                            if (sliceSize <= 12)
+                            {
+                                // We have a valid slice.
+                            }
+                        }
+
+
+
+            }
 
         }
     }
