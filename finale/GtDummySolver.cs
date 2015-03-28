43427cc9f40f27773612a99e861a494c36dbcb9d
Guillaume Turri <guillaume.turri@gmail.com>
2015-03-28 14:34:26 +0100

    Add GtDummySolver

---
+++ finale/GtDummySolver.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace finale
+{
+	public class GtDummySolver
+	{
+		private Problem _problem;
+
+		public GtDummySolver (Problem problem)
+		{
+			_problem = problem;
+		}
+
+		public Solution Solve(){
+			Solution sol = new Solution(_problem);
+			for (int tour = 0; tour < _problem; tour++) {
+				List<int> commands = new List<int> ();
+				for (int b=0; b < _problem.NbAvailableBallons; b++) {
+					commands.Add (0);
+				}
+				sol.Moves.Add (commands.ToArray ());
+			}
+		}
+	}
+}
+
