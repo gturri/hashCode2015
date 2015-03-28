885b41257aa3f3991acb0f08605427d2b2d05c2e
Alexandre Olivier <a.olivier@criteo.com>
2015-03-28 11:51:19 +0100

    Added missing file.

---
+++ finale/MapPrinter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finale
+{
+    public class MapPrinter
+    {
+        public void PrintMap(Problem problem)
+        {
+            for (int row = 0; row < problem.NbLines; row++)
+            {
+                for (int col = 0; col < problem.NbCols; col++)
+                {
+                    bool target = false;
+                    foreach (var localisation in problem.Targets)
+                        if (localisation.Col == col && localisation.Line == row)
+                            target = true;
+
+                    if (target)
+                        Console.Write("x");
+                    else
+                        Console.Write(".");
+
+                        
+                }
+                Console.Write("\n");
+            }
+
+
+            Console.WriteLine("press key to continue...");
+            Console.ReadKey();
+        }
+
+
+    }
+}
