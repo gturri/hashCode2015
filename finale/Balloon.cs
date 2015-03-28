eda5b6898a7bbb5a399b2b518a9bbce0639a1235
Alexandre Olivier <a.olivier@criteo.com>
2015-03-28 13:27:49 +0100

    Balloooooooooooon.

---
+++ finale/Balloon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finale
+{
+    public class Balloon
+    {
+        public int Altitude {get; set; }
+        public Localisation Location { get; set; }
+    }
+}
