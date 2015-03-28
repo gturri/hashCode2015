1dbdf6b3e819053afa508826441dabfdc1d83c98
Guillaume Turri <guillaume.turri@gmail.com>
2015-03-28 11:32:27 +0100

    Add Caze

---
+++ finale/Caze.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace finale
+{
+	public class Caze
+	{
+		public Vector[] Winds { get; private set; }
+		public bool IsTarget { get; private set; }
+
+		public int Line {get; private set;}
+		public int Column { get; private set; }
+
+		public Caze ()
+		{
+		}
+	}
+}
+
