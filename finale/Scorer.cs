94c338d3b50bda69023a88aad9ff7a95839cca94
Guillaume Turri <guillaume.turri@gmail.com>
2015-03-28 14:06:38 +0100

    Add missing file

---
+++ finale/Scorer.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+
+namespace finale
+{
+	public static class Scorer
+	{
+		public static int Score (Solution solution)
+		{
+			int result = 0;
+			List<Balloon> ballons = BuildInitialBalloonList (solution);
+			for (int tour=0; tour < solution.Moves.Count; tour++) {
+				UpdatePositions (solution, ballons, tour);
+				result += ComputeInstantScore (solution, ballons);
+			}
+			return result;
+		}
+
+		private static List<Balloon> BuildInitialBalloonList(Solution solution){
+			List<Balloon> result = new List<Balloon> ();
+			for (int b=0; b < solution.problem.NbAvailableBallons; b++) {
+				result.Add (new Balloon (0, solution.problem.DepartBallons));
+			}
+			return result;
+		}
+
+		private static void UpdatePositions(Solution solution, List<Balloon> ballons, int tour){
+			UpdateAltitudes (solution, ballons, tour);
+			UpdateLatLon (solution, ballons);
+		}
+
+		private static void UpdateAltitudes(Solution solution, List<Balloon> ballons, int tour){
+			int[] deltaHs = solution.Moves [tour];
+			System.Diagnostics.Debug.Assert (deltaHs.Length == ballons.Count);
+
+			for (int i=0; i < deltaHs.Length; i++) {
+				int prevAltitude = ballons [i].Altitude;
+
+				if (deltaHs [i] > 1 || deltaHs [i] < -1) {
+					throw new Exception ("deltaH out of range");
+				}
+
+				ballons [i].Altitude += deltaHs [i];
+				if (ballons [i].Altitude > solution.problem.NbAltitudes) {
+					throw new Exception ("Ballon is too high");
+				}
+
+				if (prevAltitude != 0 && ballons [i].Altitude == 0) {
+					throw new Exception ("Ballon can't land");
+				}
+			}
+		}
+
+		private static void UpdateLatLon(Solution solution, List<Balloon> ballons){
+			foreach (var ballon in ballons) {
+				Vector move = solution.problem.GetCaze (ballon.Location).Winds [ballon.Altitude];
+				int newLine = ballon.Location.Line + move.DeltaRow;
+				int newCol = (ballon.Location.Col + move.DeltaCol) % solution.problem.NbCols;
+				ballon.Location = new Localisation (newLine, newCol);
+			}
+		}
+
+		private static int ComputeInstantScore(Solution solution, List<Balloon> ballons){
+			//TODO
+			return 0;
+		}
+	}
+}
+
