9e0ea5035baeee296ffce59873339a9a21f05202
Raphael Vandon <vandonr@gmail.com>
2015-04-04 00:51:24 +0200

    clean

--- finale/Solution.cs
+++ finale/Solution.cs
@@ -8,20 +8,11 @@ namespace finale
         /// <summary> first index is turn, second is balloon </summary>
 		public int[,] Moves = new int[400, 53];
 
-		public int currentTurn = 0;
 		public Problem problem { get; private set; }
 
 		public Solution (Problem problem)
 		{
 			this.problem = problem;
-		}
-
-		public void RegisterBaloonMove(int ballonIdx, int move)
-		{
-			System.Diagnostics.Debug.Assert (move < 2 && move > -2);
-			System.Diagnostics.Debug.Assert (ballonIdx >= 0 && ballonIdx < 53);
-
-			Moves [currentTurn,ballonIdx] = move;
 		}
 	}
 }
